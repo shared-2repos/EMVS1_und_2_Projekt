bc8c8280fc85a288328992e50eb0d829c7788abb
diego2000000 <diego.rombaldoni@gmail.com>
2026-03-05 12:30:33 +0100

    createt tool material class

---
+++ src/main/java/com/mittelaltermod/item/ModToolMaterials.java
@@ -0,0 +1,8 @@
+package com.mittelaltermod.item;
+
+import net.minecraft.world.item.ToolMaterial;
+
+public class ModToolMaterials {
+
+    public static final ToolMaterial SILVER = new ToolMaterial(null, 0, 0, 0, 0, null);
+}
