f22f5c6f2214563f43071ba15dd7676e2cd597c3
diego2000000 <diego.rombaldoni@gmail.com>
2026-03-17 15:34:12 +0100

    worldgen works

--- src/main/java/com/mittelaltermod/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/mittelaltermod/worldgen/ModConfiguredFeatures.java
@@ -1,19 +1,34 @@
 package com.mittelaltermod.worldgen;
 
+import java.util.List;
+
 import com.mittelaltermod.MittelalterMod;
+import com.mittelaltermod.block.ModBlocks;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 
 public class ModConfiguredFeatures {
 
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_SILVE_ORE_KEY = registerKey("silver_ore");
+
     public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
 
+        RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+
+        List<OreConfiguration.TargetBlockState> overworldSilverOre = List.of(
+                OreConfiguration.target(stoneReplaceables, ModBlocks.SILVER_ORE.get().defaultBlockState()));
+
+        register(context, OVERWORLD_SILVE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldSilverOre, 9));
     }
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
--- src/main/java/com/mittelaltermod/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/mittelaltermod/worldgen/ModPlacedFeatures.java
@@ -9,14 +9,21 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
 public class ModPlacedFeatures {
+    public static final ResourceKey<PlacedFeature> SILVER_ORE_PLACED_KEY = registerKey("silver_ore_placed");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeature = context.lookup(Registries.CONFIGURED_FEATURE);
+        register(context, SILVER_ORE_PLACED_KEY,
+                configuredFeature.getOrThrow(ModConfiguredFeatures.OVERWORLD_SILVE_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(12,
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
