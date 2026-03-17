15859c5f058b10aa115648e23f0916860d3c3c24
diego2000000 <diego.rombaldoni@gmail.com>
2026-03-17 08:56:06 +0100

    blocktagprovider works

--- src/main/java/com/mittelaltermod/datagen/DataGenerators.java
+++ src/main/java/com/mittelaltermod/datagen/DataGenerators.java
@@ -11,6 +11,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
@@ -20,7 +21,7 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 @EventBusSubscriber(modid = MittelalterMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
     @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
+    public static <T> void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator(); // generate files
         PackOutput packOutput = generator.getPackOutput(); // output files
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper(); // chek if files already exists
@@ -31,5 +32,10 @@ public class DataGenerators {
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new,
                         LootContextParamSets.BLOCK)),
                 lookupProvider));
+
+        BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, MittelalterMod.MODID,
+                existingFileHelper);
+        generator.addProvider(event.includeServer(), blockTagsProvider);
+
     }
 }
--- src/main/java/com/mittelaltermod/datagen/ModBlockStateProvider.java
+++ src/main/java/com/mittelaltermod/datagen/ModBlockStateProvider.java
@@ -1,28 +1,5 @@
 package com.mittelaltermod.datagen;
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-
-import com.mittelaltermod.block.modBlock;
-
-import net.minecraft.client.data.models.BlockModelGenerators;
-import net.minecraft.client.data.models.ItemModelOutput;
-import net.minecraft.client.data.models.blockstates.BlockModelDefinitionGenerator;
-import net.minecraft.client.data.models.model.ModelInstance;
-import net.minecraft.resources.Identifier;
-
-public class ModBlockStateProvider extends BlockModelGenerators {
-
-    public ModBlockStateProvider(Consumer<BlockModelDefinitionGenerator> p_378137_, ItemModelOutput p_378502_,
-            BiConsumer<Identifier, ModelInstance> p_378240_) {
-        super(p_378137_, p_378502_, p_378240_);
-    }
-
-    @Override
-    public void run() {
-        this.createTrivialBlock(modBlock.SILVER_BLOCK, null);
-
-        super.run();
-    }
+public class ModBlockStateProvider {
 
 }
