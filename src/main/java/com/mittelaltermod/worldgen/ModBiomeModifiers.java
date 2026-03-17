c51d6eb64a1bd3a543cec99f5aa0171b38fb29bc
diego2000000 <diego.rombaldoni@gmail.com>
2026-03-17 11:42:52 +0100

    addedbiomes to datagen

--- src/main/java/com/mittelaltermod/worldgen/ModBiomeModifiers.java
+++ src/main/java/com/mittelaltermod/worldgen/ModBiomeModifiers.java
@@ -1,5 +1,24 @@
 package com.mittelaltermod.worldgen;
 
+import com.mittelaltermod.MittelalterMod;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.Identifier;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModBiomeModifiers {
 
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeature = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS,
+                Identifier.fromNamespaceAndPath(MittelalterMod.MODID, name));
+    }
 }
